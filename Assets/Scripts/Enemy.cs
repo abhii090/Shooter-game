3460bee15fc660349b76674dda2c199d4c78902d
Abhi <abhishek.bedsur@indivillage.co>
2025-11-04 15:14:46 +0530

    enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform target;
+    public float speed = 3f;
+    public float rotateSpeed = 0.0025f;
+    private Rigidbody2D rb;
+
+ private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+ private void Update()
+    {
+       if(!target) {
+        GetTarget();
+       } else {
+
+        RotateTowardsTarget();
+       }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.velocity = transform.up * speed;
     }
-}
+    private void RotateTowardsTarget() {
+        Vector2 targetDirection = target.position - transform.position;
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+        Quaternion q = Quaternion.Euler(new Vector3(0 , 0 , angle)); 
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, q , rotateSpeed);
+    }
+
+    private void GetTarget () {
+
+     target = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+    
+    
+  }
